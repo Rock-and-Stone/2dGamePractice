95c41cec7632dbc87fb7f17792fab9d52bc12988
Rock-and-Stone <wodyd031@outlook.kr>
2022-02-01 18:13:21 +0900

    no message

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,6 +9,10 @@ public class RubyController : MonoBehaviour
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +20,6 @@ public class RubyController : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
-
-        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -31,10 +33,23 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if(isInvincible)
+		{
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0) isInvincible = false;
+		}
     }
 
     public void ChangeHealth(int amount)
 	{
+        if(amount <0)
+		{
+            if (isInvincible) return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+		}
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
 	}
